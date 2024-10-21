4e93a7125b3cd421a73b8728c1b5bb960380d042
HuyLeDac <huyledac04@gmail.com>
2024-10-21 17:43:29 +0200

    added some lines on app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 import os
 
-file_path = os.path.join("inputs", "architecture_description.txt")
-architecture_description = open(file_path, "r")
+architecture_description_path = os.path.join("inputs", "architecture_description.txt")
+architecture_description = open(architecture_description_path, "r")
 
-print(architecture_description.read())
