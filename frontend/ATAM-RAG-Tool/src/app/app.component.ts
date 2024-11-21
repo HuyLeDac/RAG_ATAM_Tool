4ae3835fd2ca657d8c8832d9042ed0da8a6686fe
HuyLeDac <huyledac04@gmail.com>
2024-11-21 17:40:55 +0100

    fixed angular project

--- frontend/ATAM-RAG-Tool/src/app/app.component.ts
+++ frontend/ATAM-RAG-Tool/src/app/app.component.ts
@@ -1,23 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { HeaderComponent } from './header/header.component';
-import { ColumnsComponent } from './columns/columns.component';
-import { FooterComponent } from './footer/footer.component';
-
-
+import { HeaderComponent } from "./header/header.component";
+import { ColumnsComponent } from "./columns/columns.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ColumnsComponent, FooterComponent], // Correct imports
+  imports: [RouterOutlet, HeaderComponent, ColumnsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] // Corrected to styleUrls (plural)
+  styleUrl: './app.component.scss'
 })
-
 export class AppComponent {
-
   title = 'ATAM-RAG-Tool';
-
 }
